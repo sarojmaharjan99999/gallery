67ac45d8be0eed6beced0d642742928bbeed4d5c
Saroj Maharjan <sarojmhrzn@gmail.com>
2016-09-24 22:36:52 +0545

    next and prev is working but its looping continuosly

--- js/script.js
+++ js/script.js
@@ -4,13 +4,40 @@
 
 "use strict";
 
- var listSlides = document.querySelector(".list-slides");
- var li = listSlides.querySelectorAll("li");
- var lastChild = li[li.length-1];
- var firstChild = document.querySelectorAll(".list-slides > li")[0];
+ var prev = document.querySelector(".prev");
+ var next = document.querySelector(".next");
+ var featureImg = document.querySelector(".list-slides img");
+ var imgArray = ['images/img1.jpg' , 'images/img2.jpg' , 'images/img3.jpg'];
+ var i = 0;
 
+    // setInterval(function () {
+    //     featureImg.src = imgArray[i];
+    //     i++;
+    //     if (i==3)  i=0;
+    // }1000);
 
- setInterval(function(){
-     firstChild.style.margin = "0px 0px 0px -300px";
-     lastChild.style.margin = "0px";
- }, 3000);
+ function newImage() {
+     featureImg.setAttribute('src',imgArray[i]);
+     i++;
+
+     if (i>= imgArray.length) {
+         i=0;
+     }
+ }
+
+ function prevImg() {
+     i--;
+
+     if (i<0){
+         i=imgArray.length-1;
+     }
+     featureImg.setAttribute('src',imgArray[i]);
+ }
+
+ prev.onclick = function () {
+     prevImg();
+ };
+
+ next.onclick = function () {
+     newImage();
+ };
